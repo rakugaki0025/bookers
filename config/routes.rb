4f7cb23a581afa9d2fa6ead7b57f241f0ea7a546
rakugaki0025 <135841624+rakugaki0025@users.noreply.github.com>
2023-07-07 14:49:46 +0000

    2page sakusei

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get '/top' => 'homes#top'     # topページへ飛ぶために必要
+  get '/books' => 'books#top'   # topページへ飛ぶために必要(～/表示先) => 'サイト名'
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
