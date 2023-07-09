01aa802aba1dd5dbbcdd91900ab018d93e7b146c
rakugaki0025 <135841624+rakugaki0025@users.noreply.github.com>
2023-07-09 00:29:56 +0000

    list start ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'lists/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/edit'
   get '/top' => 'homes#top'     # topページへ飛ぶために必要
   get '/books' => 'books#top'   # topページへ飛ぶために必要(～/表示先) => 'サイト名'
-  
+  post 'lists' => 'lists#create' # 保存機能のため送信側？
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
