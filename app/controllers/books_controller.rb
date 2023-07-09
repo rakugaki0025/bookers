e92d6227d372c6f8fcdeb55c5f7be8b4fd834627
rakugaki0025 <135841624+rakugaki0025@users.noreply.github.com>
2023-07-09 11:49:00 +0000

    redo start_02

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,19 @@
 class BooksController < ApplicationController
   def top
   end
+  
+  def index
+    @books = Book.new
+  end
+  
+  
+  
+  private
+  # ストロングパラメータ
+  # ここから下に記述することでメソッドの呼び出しに制限
+  def books_params
+    params.require(:lists).permit(:title, :body)
+     # params.require(:モデル名).permit(:保存を許可するカラム指定)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get '/top' => 'homes#top'     # topページへ飛ぶために必要
-  get '/books' => 'books#top'   # topページへ飛ぶために必要(～/表示先) => 'サイト名'
-  post 'lists' => 'lists#create' # 保存機能のため送信側？
-  
+  get '/top' => 'homes#top'      # topページへ飛ぶために必要
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
