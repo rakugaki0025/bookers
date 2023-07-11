c55ada79774dfad80f5b7ae880786c612a599f7a
rakugaki0025 <135841624+rakugaki0025@users.noreply.github.com>
2023-07-11 12:27:58 +0000

    toukou finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,16 +4,30 @@ class BooksController < ApplicationController
   
   def index
     @books = Book.new
+        # インスタンス変数 = モデル名 空オブジェクト 新規作成
+        # 左の箱に右を格納
+  end
+  
+  def create
+        # １.&2. データを受け取り新規登録するためのインスタンス作成
+        # list = List.new(list_params)
+        # privateメソッドの名前は、「モデル名_params」とする
+      book = Book.new(book_params)
+        # ローカル変数 = モデル名.newと一緒
+      book.save
+        # saveメソッド
+      redirect_to '/books/show'
+        # 遷移する先
   end
   
   
-  
   private
-  # ストロングパラメータ
-  # ここから下に記述することでメソッドの呼び出しに制限
-  def books_params
-    params.require(:lists).permit(:title, :body)
-     # params.require(:モデル名).permit(:保存を許可するカラム指定)
+        # ストロングパラメータ
+        # ここから下に記述することでメソッドの呼び出しに制限
+  def book_params
+        # モデル名と一緒
+    params.require(:book).permit(:title, :body)
+        # params.require(:モデル名).permit(:保存を許可するカラム指定)
   end
   
 end
