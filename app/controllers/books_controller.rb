e958cc94faeb2b73b68952e12930dfcbd6d1a2c5
rakugaki0025 <135841624+rakugaki0025@users.noreply.github.com>
2023-07-16 05:42:38 +0000

    delete finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,10 +3,11 @@ class BooksController < ApplicationController
   end
   
   def index
-    @book = Book.new
+      @book = Book.new
         # インスタンス変数 = モデル名 空オブジェクト 新規作成
         # 左の箱に右を格納
-    @books = Book.all 
+        # インスタンス名はなんでもいい,空のオブジェクトもなんでもいい
+      @books = Book.all 
   end
   
   def create
@@ -19,16 +20,31 @@ class BooksController < ApplicationController
         # saveメソッド
         # redirect_to '/books/show'
       redirect_to book_path(book.id)
-        # 遷移する先
+        # 遷移する先 book(名称なんでも可)_id_投稿XXページ
   end
   
+  def destroy
+      @book = Book.find(params[:id])
+        # データ（レコード）を1件取得
+      
+      @book.destroy
+        # データ（レコード）を削除
+      
+      redirect_to '/books'
+        # 投稿一覧画面へリダイレクト  
+  end
+  
+  
   def show
-    @book = Book.find(params[:id])
-     # @book = Book.find(params[:投稿ページ])
+      @book = Book.find(params[:id])
+        # @book = Book.find(params[:投稿ページ])
   end
     
   def edit
   end
+  
+  
+  
   
   
   private
@@ -36,7 +52,7 @@ class BooksController < ApplicationController
         # ここから下はこのcontrollerの中でしか呼び出せません
   def book_params
         # モデル名と一緒
-    params.require(:book).permit(:title, :body)
+      params.require(:book).permit(:title, :body)
         # params.require(:モデル名).permit(:保存を許可するカラム指定)
   end
   
